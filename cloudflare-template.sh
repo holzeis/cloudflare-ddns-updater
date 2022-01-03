952115980e60daba00831739b3342f0161cf500d
Richard Holzeis <richard.holzeis@at.ibm.com>
2022-01-03 07:38:36 +0100

    fix: changed bash to sh and logger to echo

--- cloudflare-template.sh
+++ cloudflare-template.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-## change to "bin/sh" when necessary
+#!/bin/sh
 
 cf_email=$1
 cf_apikey=$2
@@ -29,7 +28,7 @@ talktoken=$talk_token                              # API Token for user name
 ip=$(curl -s https://api.ipify.org || curl -s https://ipv4.icanhazip.com/)
 
 if [ "${ip}" == "" ]; then 
-  logger -s "DDNS Updater: No public IP found"
+  echo "DDNS Updater: No public IP found"
   exit 1
 fi
 
@@ -46,7 +45,7 @@ fi
 ## Seek for the A record
 ###########################################
 
-logger "DDNS Updater: Check Initiated"
+echo "DDNS Updater: Check Initiated"
 record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A&name=$record_name" \
                       -H "X-Auth-Email: $auth_email" \
                       -H "$auth_header $auth_key" \
@@ -56,7 +55,7 @@ record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identi
 ## Check if the domain has an A record
 ###########################################
 if [[ $record == *"\"count\":0"* ]]; then
-  logger -s "DDNS Updater: Record does not exist, perhaps create one first? (${ip} for ${record_name})"
+  echo "DDNS Updater: Record does not exist, perhaps create one first? (${ip} for ${record_name})"
   exit 1
 fi
 
@@ -66,7 +65,7 @@ fi
 old_ip=$(echo "$record" | sed -E 's/.*"content":"(([0-9]{1,3}\.){3}[0-9]{1,3})".*/\1/')
 # Compare if they're the same
 if [[ $ip == $old_ip ]]; then
-  logger "DDNS Updater: IP ($ip) for ${record_name} has not changed."
+  echo "DDNS Updater: IP ($ip) for ${record_name} has not changed."
   exit 0
 fi
 
@@ -89,7 +88,7 @@ update=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_iden
 ###########################################
 case "$update" in
 *"\"success\":false"*)
-  logger -s "DDNS Updater: $ip $record_name DDNS failed for $record_identifier ($ip). DUMPING RESULTS:\n$update"
+  echo "DDNS Updater: $ip $record_name DDNS failed for $record_identifier ($ip). DUMPING RESULTS:\n$update"
   if [[ $talkroomuri != "" ]]; then
 
     curl -L -X POST $talkroomuri \
@@ -102,7 +101,7 @@ case "$update" in
   fi
   exit 1;;
 *)
-  logger "DDNS Updater: $ip $record_name DDNS updated."
+  echo "DDNS Updater: $ip $record_name DDNS updated."
   if [[ $talkroomuri != "" ]]; then
     curl -L -X POST $talkroomuri \
     -H "Content-Type: application/json" \
